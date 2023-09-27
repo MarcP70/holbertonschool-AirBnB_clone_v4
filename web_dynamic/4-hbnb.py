f4cd11265fcb219ae35fae0fb34200790246a50d
MarcP70 <marcpourias@yahoo.fr>
2023-09-27 12:01:14 +0200

    fix file 4-hbnb.py that change the function

--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -6,7 +6,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template
 import uuid
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
@@ -22,11 +22,6 @@ def close_db(error):
 @app.route('/4-hbnb/', methods=['GET', 'POST'], strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
-    if request.method == 'POST':
-        # For POST request, handle places_search with amenities
-        amenities = request.get_json().get('amenities', [])
-        filtered_places = get_filtered_places(amenities)
-        return jsonify([place.to_dict() for place in filtered_places])
 
     # For GET request, render the template with data
     states = storage.all(State).values()
@@ -50,36 +45,6 @@ def hbnb():
                            places=places,
                            cache_id=cache_id
                            )
-
-
-def get_filtered_places(amenities):
-    """ Filter places based on amenities """
-    if not amenities:
-        return storage.all(Place).values()
-
-    filtered_places = set()
-    for amenity_id in amenities:
-        amenity = storage.get(Amenity, amenity_id)
-        if amenity:
-            for place in amenity.places:
-                filtered_places.add(place)
-
-    return list(filtered_places)
-
-
-@app.route('/api/v1/places_search/', methods=['POST'])
-def places_search():
-    """ Search for places """
-    data = request.get_json()
-
-    places = storage.all(Place).values()
-    places = sorted(places, key=lambda k: k.name)
-
-    places_list = []
-    for place in places:
-        places_list.append(place.to_dict())
-
-    return jsonify(places_list)
 
 if __name__ == "__main__":
     """ Main Function """
